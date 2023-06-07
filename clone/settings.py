ca472e9657b73b7e9b67a5c354295da15268117c
zakir110901 <126509575+zakir110901@users.noreply.github.com>
2023-06-07 23:06:33 +0530

    Update settings.py

--- clone/settings.py
+++ clone/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(j-j%m+gxj#2@21zq6x^s5pgv*!rs7^j+d!)%i-xwandxc&@(7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
